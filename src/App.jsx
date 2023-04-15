35a8342ac7aa232971086b1f5a425667fe8c92df
Ajarek <ajarek@poczta.onet.pl>
2023-04-16 00:56:44 +0200

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import Dice from './component/Dice'
+function App() {
+  
+
+  return (
+    <div className="App">
+      <Dice/>
+      <Dice/>
+      <Dice/>
+      <Dice/>
+      <Dice/>
+      <Dice/>
+      <Dice/>
+      <Dice/>
+      <Dice/>
+      <Dice/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/component/Dice.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Dice = () => {
+  return (
+    <div className='dice'>
+      <div className="dot"></div>
+      <div className="dot"></div>
+      <div className="dot"></div>
+      <div className="dot"></div>
+      <div className="dot"></div>
+      <div className="dot"></div>
+    </div>
+  )
+}
+
+export default Dice
